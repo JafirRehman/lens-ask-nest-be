9091909a919408136528b238a1ece551dc0591fb
JafirRehman <mjafirrehman@gmail.com>
2025-09-18 03:08:51 +0500

    update message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello jafir khan!';
+    return 'Hello jafir Rehman!';
   }
 }
