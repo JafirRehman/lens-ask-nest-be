fc5e8f90b20c819d13d30d14f1568392b9a69793
JafirRehman <mjafirrehman@gmail.com>
2025-09-18 01:48:04 +0500

    set config

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,19 +16,27 @@ import { OrderItemsModule } from './order-items/order-items.module';
 import { Cart } from './carts/entities/cart.entity';
 import { CartItem } from './cart-items/entities/cart-item.entity';
 import { OrderItem } from './order-items/entities/order-item.entity';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+  
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 'abc123',
-      username: 'postgres',
-      entities: [User, Subscriber, Order, Product, Cart, CartItem, OrderItem, Order],
-      database: 'lens-ask',
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        password: configService.get('DB_PASS'),
+        username: configService.get('DB_USER'),
+        entities: [User, Subscriber, Order, Product, Cart, CartItem, OrderItem],
+        database: configService.get('DB_NAME'),
       synchronize: true,
     }),
+  }),
     UsersModule,
     SubscribersModule,
     OrdersModule,
